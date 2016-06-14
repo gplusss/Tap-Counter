5731958e32785e11f124668d1daabe4eacde86c7
Vladimir Saprykin <gplusss@gmail.com>
2016-06-14 09:10:41 +0300

    tap button

--- TAP COUNTER/ViewController.swift
+++ TAP COUNTER/ViewController.swift
@@ -9,16 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var count: Int = 0
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var tapLabel: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "COUNTER"
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func startTapLable(sender: UIButton) {
+        
+        count += 1
+        countLabel.text = String(count)
+        print(countLabel.text!)
+    }
+    
+    
 
 
 }
